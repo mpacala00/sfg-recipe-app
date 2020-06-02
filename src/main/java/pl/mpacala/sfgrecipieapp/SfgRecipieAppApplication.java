7d97adc3ee13e4ffa1f668922c69895aaf63b7c0
mpacala00 <mpacala00@gmail.com>
2020-06-02 12:28:35 +0200

    Initial commit.

---
+++ src/main/java/pl/mpacala/sfgrecipieapp/SfgRecipieAppApplication.java
@@ -0,0 +1,13 @@
+package pl.mpacala.sfgrecipieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgRecipieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgRecipieAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/mpacala/sfgrecipieapp/SfgRecipieAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mpacala.sfgrecipieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipieAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
