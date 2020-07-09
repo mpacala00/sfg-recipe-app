4f514cdd276e4691cba928dc7641b1708dcfb0ac
mpacala00 <mpacala00@gmail.com>
2020-07-09 23:33:12 +0200

    Adding 2 query methods in Category and UOM repos.

--- src/main/java/pl/mpacala/sfgrecipieapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/pl/mpacala/sfgrecipieapp/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,10 @@ package pl.mpacala.sfgrecipieapp.repositories;
 import org.springframework.data.repository.CrudRepository;
 import pl.mpacala.sfgrecipieapp.model.UnitOfMeasure;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    //query method - does not require implementation thanks to JPA
+    Optional<UnitOfMeasure> findByUnit(String unit);
 }
