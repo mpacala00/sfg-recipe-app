4569eddab94c88083180134114d029d856e3fbc8
mpacala00 <mpacala00@gmail.com>
2021-07-07 21:25:41 +0200

    Adding property files for internationalization

--- src/main/java/pl/mpacala/sfgrecipieapp/SfgRecipieAppApplication.java
+++ src/main/java/pl/mpacala/sfgrecipieapp/SfgRecipieAppApplication.java
@@ -1,7 +1,9 @@
 package pl.mpacala.sfgrecipieapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SfgRecipieAppApplication {
